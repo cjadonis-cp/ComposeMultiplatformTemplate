dd090bc6a0321dea893ed5a8e93a0317f7797ab9
cjadonis <christopheradonis0320@gmail.com>
2026-04-22 00:20:56 +0800

    chore: add docs to gitignore, fix iOS simulator run config

--- composeApp/src/commonMain/kotlin/org/adonis/project/core/database/DatabaseDriverFactory.kt
+++ composeApp/src/commonMain/kotlin/org/adonis/project/core/database/DatabaseDriverFactory.kt
@@ -2,6 +2,7 @@ package org.adonis.project.core.database
 
 import app.cash.sqldelight.db.SqlDriver
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect class DatabaseDriverFactory {
     fun create(): SqlDriver
 }
--- composeApp/src/iosMain/kotlin/org/adonis/project/core/database/DatabaseDriverFactory.ios.kt
+++ composeApp/src/iosMain/kotlin/org/adonis/project/core/database/DatabaseDriverFactory.ios.kt
@@ -3,6 +3,7 @@ package org.adonis.project.core.database
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class DatabaseDriverFactory {
     actual fun create(): SqlDriver {
         return NativeSqliteDriver(
